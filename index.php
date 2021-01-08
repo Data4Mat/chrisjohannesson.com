9943be94420fc95f8c743dc8c79dbe97db6b7b99
ChrisJohDev <chris@chrisjohannesson.com>
2021-01-08 11:16:00 +0100

    temp update

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Chris Johannesson</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.0/css/bulma.min.css">
+    <link rel="icon" type="image/png" href="/images/cj-icon_512x512.png" />
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/css/styles.css">
+    <!--<script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>-->
+    <!-- My personal kit at fontawsome.com -->
+    <script src="https://kit.fontawesome.com/71a1019fc2.js" crossorigin="anonymous"></script>
+    <script src="/src/js/helper.js"></script>
+    <!-- <link rel="stylesheet" href="styles/debug.css"> -->
+</head>
+
+<body>
+    <nav id="navbar" class="navbar" role="navigation" aria-label="main navigation"></nav>
+    <div id="container" class="container align_center" width="100vw" height="100vh">
+        <h1 class="title">Chris Johannesson - $page</h1>
+        <p>Welcome to my home on the internet.</p>
+        <br />
+        <br />
+        <div class="field">
+                                <label class="label">Email</label>
+                                <div class="control has-icons-left">
+                                    <input class="input" type="email" placeholder="e.g. name@domain.com" required>
+                                    <span class="icon is-small is-left">
+                                        <i class="fa fa-envelope"></i>
+                                    </span>
+                                </div>
+                            </div>
+
+        <div class="tile is-ancestor">
+            <div class="tile is-parent is-6" style="max-height: 50vh">
+                <figure class="tile is-child image is-4by3">
+                    <img class="me_class" src="/images/chris_johannesson2.jpg" />
+                </figure>
+            </div>
+            <div class="tile is-parent is-4">
+                <figure class="tile is-child image is-3by4">
+                    <img class="me_class" src="/images/chris_bass.jpg" />
+                </figure>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p class="copyright_class">Data4Mat &copy; 2020<br>Chris Johanneson</p>
+        </div>
+    </footer>
+    <script src="/src/js/main.js"></script>
+    <script src="/src/js/bulma.js"></script>
+</body>
+
+</html>
